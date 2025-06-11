987e862c0fb93ae19f580f3d4758517f56553c83
vt-abreu <vittorabreustds@gmail.com>
2025-06-11 21:21:53 +0100

    Novo exercício

--- first-exs/pythagoras.py
+++ first-exs/pythagoras.py
@@ -1,1 +1,10 @@
 #Fazer um programa que calcule a hipotenusa
+import math
+from math import hypot
+
+c = float(input('Digite o valor do cateto adjacente: '))
+co = float(input('Digite o valor do cateto oposto: '))
+h = hypot(c , co)
+print('A hipotenusa corresponde a: {:.3f}'.format(h))
+
+
