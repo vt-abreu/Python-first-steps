9fb8ac8612be239166646cee54e4ddc13099d50c
vt-abreu <vittorabreustds@gmail.com>
2025-06-12 03:11:00 +0100

    Corrigi mais erros

--- first-exs/pythagoras.py
+++ first-exs/pythagoras.py
@@ -6,7 +6,6 @@ c = float(input('Digite o valor do cateto adjacente: '))
 co = float(input('Digite o valor do cateto oposto: '))
 h = hypot(c , co)
 
-
 print('A hipotenusa corresponde a: {:.3f}'.format(h))
 
 
